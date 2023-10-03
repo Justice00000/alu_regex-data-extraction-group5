edaa78120196c269715ca844f6b67d1593e5b9f2
CeeJay <j.chukwuony@alustudent.com>
2023-10-03 21:08:01 +0300

    Update ingredients_matches.py

--- ingredients_matches.py
+++ ingredients_matches.py
@@ -1,18 +1,11 @@
 import re
 
-# Sample text containing ingredient lists
-text = """
-- Ingredients: "Tomatoes, Cheese, Pepperoni, Dough"
-- Ingredients: "Eggs, Flour, Sugar, Milk, Butter"
-"""
+raw_data = "Ingredients: 'Tomatoes, Cheese, Pepperoni, Dough', 'Eggs, Flour, Sugar'"
 
-# Define regular expressions for ingredient lists
-ingredients_pattern = r'"Ingredients: "(.*?)"'
+ingredients_pattern = r"'(.*?)'"
+ingredients_matches = re.findall(ingredients_pattern, raw_data)
 
-# Find all matches for ingredient lists
-ingredients_matches = re.findall(ingredients_pattern, text)
-
-print("\nExtracted Ingredient Lists:")
 for ingredients in ingredients_matches:
     ingredients_list = [ingredient.strip() for ingredient in ingredients.split(',')]
-    print("Ingredients:", ingredient_list)
+    print("Ingredients:", ingredients_list)
+
