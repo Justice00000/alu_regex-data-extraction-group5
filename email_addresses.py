c53326b8ef2a19db26d88e498ec8f4ffefcd2868
kingdavidch <kingdavidsapps@gmail.com>
2023-10-04 09:55:56 +0000

    king

---
+++ email_addresses.py
@@ -0,0 +1,20 @@
+import re
+
+# Sample text containing email addresses
+text = """
+Email addresses:
+- j.chukwuony@example.com
+- alice.smith@email.co.uk
+- support@company.org
+"""
+
+# Define a regular expression pattern for email addresses
+email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+# Find all matches for email addresses
+email_matches = re.findall(email_pattern, text)
+
+# Print the extracted email addresses
+print("\nExtracted Email Addresses:")
+for email in email_matches:
+    print("Email:", email)
